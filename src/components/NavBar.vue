b63d01c626a2d486f374fe9873adf4957c679b4d
eoliva50 <eoliva50>
2023-12-03 22:37:33 -0600

    Added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,32 @@
 <script setup>
   import { ref } from 'vue'
-  const brand = ref('Angelicpedia')
+  import { useAuth } from '@/composables/useAuth'
+
+  const { isAuthenticated, logout, user } = useAuth()
+
+  const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
   <nav>
     <div class="wrapper">
-      <div class="brand">
+      <RouterLink :to="{ name: 'Home' }" class="brand">
         <span class="brand-title">{{ brand }}</span>
-      </div>
+      </RouterLink>
       <div class="menu">
-        <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-login">Logout</a>
+        <p v-show="isAuthenticated" class="px-2 py-4">
+          Welcome back
+          <strong>
+            <i>{{ user?.email }}</i>
+          </strong>
+        </p>
+        <div v-if="isAuthenticated">
+          <RouterLink :to="{ name: 'Settings' }" href="#" class="menu-item">Settings</RouterLink>
+          <button class="menu-logout" @click="logout">Logout</button>
+        </div>
+        <div v-else>
+          <RouterLink :to="{ name: 'Login' }" href="#" class="menu-login">Login</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
@@ -29,11 +44,17 @@
       }
       .menu {
         @apply flex gap-2;
+        & div {
+          @apply py-2;
+        }
         &-item {
           @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
         }
         &-login {
-          @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+          @apply rounded-md bg-green-500 px-4 py-2 text-red-100 hover:bg-green-700;
+        }
+        &-logout {
+          @apply mx-2 rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
         }
       }
     }
